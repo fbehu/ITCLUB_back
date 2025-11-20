87c2b345542b3830b6765d75679756489216857b
fbehu <fbehu68@gmail.com>
2025-11-20 14:30:34 +0000

    change

--- config/settings.py
+++ config/settings.py
@@ -75,6 +75,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://www.ubsitclub.uz",
     "https://api.ubsitclub.uz",
     "http://91.210.106.114",
+    "https://itclub.cyberrs.uz",
     # "http://127.0.0.1:8080",
     # "http://localhost:8080",
 ]

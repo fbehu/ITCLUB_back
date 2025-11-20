0ed6e58065dd70cb0a6030889e79a0bb34854811
fbehu <fbehu68@gmail.com>
2025-11-20 06:03:11 +0000

    Change

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&gis75^+e$m4#f66)h%szqim!l$yvr5qj_i#8)+s!)y1-abrba'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["ubsitclub.uz", "www.ubsitclub.uz", "api.ubsitclub.uz", "127.0.0.1", "localhost", "91.210.106.114"]
+ALLOWED_HOSTS = ["itclub.cyberrs.uz", "ubsitclub.uz", "www.ubsitclub.uz", "api.ubsitclub.uz", "127.0.0.1", "localhost", "91.210.106.114"]
 
 
 # Application definition

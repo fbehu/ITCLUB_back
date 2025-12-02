5d2a8229580b7dcd668f2c29e3003c25fa82902b
Cyber3113 <legenda16052007@gmail.com>
2025-12-02 09:34:13 +0500

    change profile

--- apps/groups/models.py
+++ apps/groups/models.py
@@ -17,7 +17,7 @@ class Group(Basemodel):
         ('saturday', 'Shanba'),
         ('sunday', 'Yakshanba'),
     ]
-    class_days = models.CharField(choices=CLASSDAYS, max_length=50, verbose_name="Dars kunlari")
+    # class_days = models.CharField(choices=CLASSDAYS, max_length=50, verbose_name="Dars kunlari")
     name = models.CharField(max_length=255, verbose_name="Guruh nomi")
     smena = models.CharField(max_length=500, verbose_name="Guruh Smenasi", blank=True)
     start_time = models.CharField(max_length=20, blank=True, null=True)

2dbd96a0bdc208984f1c0213d107a6d84f6c4804
Cyber3113 <legenda16052007@gmail.com>
2025-12-20 01:53:25 +0500

    o'qituvchi roli uhcun ma'lumotlarni o'zgartirish o'chirish qo'shishga ruxsat berildi

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -5,4 +5,4 @@ class IsAdmin(BasePermission):
     Faqat admin roliga ega foydalanuvchilarga ruxsat beradi.
     """
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.role == 'admin')
+        return bool(request.user and request.user.is_authenticated and request.user.role == 'admin' or request.user.role == 'teacher')
